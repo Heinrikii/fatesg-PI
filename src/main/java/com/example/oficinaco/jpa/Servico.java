41b4dc1346a440dd27e963e6e3c068220d27a4f1
Francisco Calaça <francisco@getcoders.io>
2022-10-07 19:58:53 -0300

    conclusao do crud

--- src/main/java/com/example/oficinaco/jpa/Servico.java
+++ src/main/java/com/example/oficinaco/jpa/Servico.java
@@ -3,12 +3,15 @@ package com.example.oficinaco.jpa;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Servico {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;
